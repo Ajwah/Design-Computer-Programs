361bb41f0abd53dc6d11f9744d05c2748ad4c9e2
Ajwah <johnson786.kevin+git@gmail.com>
2015-07-10 01:36:28 -0400

    Implement two functions: poker and hand_rank

---
+++ poker.py
@@ -0,0 +1,8 @@
+"Implementation of a poker game"
+def poker(hands):
+  "Return the best hand: poker([hand,...]) => hand"
+  return max(hands,hand_rank)
+
+def hand_rank(hands):
+  "Determine the rank of all the cards involved in hands"
+  return None
