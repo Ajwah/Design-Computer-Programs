a1727ada9a2c84c009f41349c771a1d881b23549
Ajwah <johnson786.kevin+git@gmail.com>
2015-07-09 22:34:21 -0400

    Implement docstring

---
+++ docstring.py
@@ -0,0 +1,14 @@
+"""
+  Concept of Doc String: https://en.wikipedia.org/wiki/Docstring
+  The purpose of this module is to experiment with the concept of docstrings under Python.
+"""
+
+class My_Class(object):
+  """This is the docstring of 'my_class'"""
+
+  def my_Method(arg):
+    """This is the docstring of 'my_Method'"""
+
+def my_Function():
+  """This is the docstring of my_Function"""
+
---
+++ test_docstring.py
@@ -0,0 +1,5 @@
+import docstring
+help(docstring)
+help(docstring.My_Class)
+help(docstring.My_Class.my_Method)
+help(docstring.my_Function)
