29a615e18284dc61117fbd6db0a3930275f0e76f
Ajwah <johnson786.kevin+git@gmail.com>
2015-07-10 01:42:54 -0400

    Implement two extra tests.

--- poker.py
+++ poker.py
@@ -12,7 +12,10 @@ def test():
   sf = "6C 7C 8C 9C TC".split()
   fk = "9D 9H 9S 9C 7D".split()
   fh = "TD TC TH 7C 7D".split()
-  assert poker([sf,fk,fh]) == sf
+  assert poker([sf,fk,fh]) == sf, "Assert that straight flush is the winning hand"
+  assert poker([fk,fh]) == fk, "Assert that four of a kind is the winning hand"
+  assert poker([fh,fh]) == fh, "Assert that two times full house produces full house as the winning hand"
+
   return "tests pass."
 
 print test()
