842033a7f2b6fc1fa42aa286f841bf8b90c4a60e
Ajwah <johnson786.kevin+git@gmail.com>
2015-07-18 06:43:06 -0400

    Integrate a time tracker function and refactor code so that zebra puzzle is a function that can be tracked. Implement variable tracker locally to zebrapuzzle()

--- Zebra Puzzle.py
+++ Zebra Puzzle.py
@@ -1,49 +1,63 @@
 import itertools
 import types
 import operator
+import time
 
-d = dict(globals())
-result = []
-def kv_test(k,v):
-    return (k not in d and
-            k not in ['d','args'] and
-            type(v) is not types.FunctionType and
-            k != 'orderings' and k != 'houses' and k!= 'result' and k != 'i' and k != 'row' and k != 's')
+def timedCall(fn, *args):
+  'Call function and report elapsed time'
+  t0 = time.clock()
+  r = fn(*args)
+  t1 = time.clock()
+  return t1-t0, r
 
-def magic_print(*args):
-    if len(args) == 0:
-        return {k:v for k,v in globals().iteritems() if kv_test(k,v)}
-    else:
-        return {k:v for k,v in magic_print().iteritems() if k in args}
+def zebra_puzzle():
+  'Solve Zebra Puzzle completely and return solution for output'
+  #List of local variables to be excluded from listing
+  exclude = ['orderings','houses','result','exclude','x', 'l', 'k', 'v']
 
-houses = range(1,6)
-orderings = list(itertools.permutations(houses))
+  def isNextTo(h,to):
+    return isRightTo(h,to) or isRightTo(to,h)
 
-def isNextTo(h,to):
-  return isRightTo(h,to) or isRightTo(to,h)
+  def isRightTo(h,to):
+    return h == to + 1
 
-def isRightTo(h,to):
-  return h == to + 1
-for (spaniard,englishman,ukranian,norwegian,japanese) in orderings:
-  if (norwegian is 1) and (norwegian is 1):
-    for (red,green,blue,ivory,yellow) in orderings:
-      if (englishman is red) and isRightTo(green,ivory):
-        for (dog,snail,fox,horse,zebra) in orderings:
-          if (spaniard is dog):
-            for (coffee,tea,milk,orangeJuice,water) in orderings:
-              if (coffee is green) and (ukranian is tea) and (milk is 3):
-                for (oldGold,kools,chesterfield,luckyStrike,parliament) in orderings:
-                  if (oldGold is snail) and (kools is yellow) and (luckyStrike is orangeJuice) and (parliament is japanese) and isNextTo(chesterfield,fox) and isNextTo(kools,horse):
-                    result.append(sorted([[v, k] for k, v in magic_print().items() if v != 'kools']))
+  houses = range(1,6)
+  orderings = list(itertools.permutations(houses))
+  result = []
 
-for row in result:
-  h = 0
-  s = ''
-  for i in row:
-    if h != i[0]:
-      print s
-      s = 'House %s populated by: '%i[0] + i[1]
-      h = i[0]
-    else:
-      s += ' - ' + i[1]
-  print s
+  for (spaniard,englishman,ukranian,norwegian,japanese) in orderings:
+    if (norwegian is 1) and (norwegian is 1):
+      for (red,green,blue,ivory,yellow) in orderings:
+        if (englishman is red) and isRightTo(green,ivory):
+          for (dog,snail,fox,horse,zebra) in orderings:
+            if (spaniard is dog):
+              for (coffee,tea,milk,orangeJuice,water) in orderings:
+                if (coffee is green) and (ukranian is tea) and (milk is 3):
+                  for (oldGold,kools,chesterfield,luckyStrike,parliament) in orderings:
+                    if (oldGold is snail) and (kools is yellow) and (luckyStrike is orangeJuice) and (parliament is japanese) and isNextTo(chesterfield,fox) and isNextTo(kools,horse):
+                      #Obtain all local variables with their corresponding values
+                      l = locals().iteritems()
+                      #Extract from l those that are relevant to the puzzle
+                      x = sorted([(k,v) for k,v in l if (k not in exclude and
+                                                        type(v) is not types.FunctionType and
+                                                        type(v) is not types.StringType)])
+                      #Include the line as a valid and possible solution
+                      result.append(sorted([[v, k] for k, v in x]))
+  return result
+
+def output():
+  t, result = timedCall(zebra_puzzle)
+  for row in result:
+    h = 0
+    s = ''
+    for i in row:
+      if h != i[0]:
+        print s
+        s = 'House %s populated by: '%i[0] + i[1]
+        h = i[0]
+      else:
+        s += ' - ' + i[1]
+    print s
+  print 'Time to execute entire function: ', t
+
+output()
